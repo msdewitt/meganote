0fb761fe8bd178ec1fb2c50878c4c6acedf17c5c
Matheu DeWitt <msdewitt@bsu.edu>
2016-06-14 09:45:31 -0400

    Clean up notes.js file

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -1,25 +1,27 @@
-angular.module('meganote.notes',[
-  'ui.router'
-])
-.config(function($stateProvider) {
-  $stateProvider
+(function(){
+  angular
+    .module('meganote.notes',['ui.router'])
+    .config(notesConfig)
+    .controller('NotesController', NotesController);
+  function notesConfig($stateProvider){
+      $stateProvider
 
-    .state('notes', {
-      url: '/notes',
-      templateUrl:'notes/notes.html' ,
-      controller: 'NotesController'
-    })
-    .state('notes.form',{
-      url:'/:noteId', //noteId is a varaiable so this says: .../noteId
-      templateUrl : 'notes/notes-form.html'
-    });
-})
-.controller('NotesController',function($scope){
-
-  $scope.notes = [];
-  $scope.note = {title:'', body:''};
-  $scope.save = function(){
-    $scope.notes.push($scope.note);
-    $scope.note ={title:'', body:''};
-  }
-});
+        .state('notes', {
+          url: '/notes',
+          templateUrl:'notes/notes.html' ,
+          controller: 'NotesController'
+        })
+        .state('notes.form',{
+          url:'/:noteId', //noteId is a varaiable so this says: .../noteId
+          templateUrl : 'notes/notes-form.html'
+        });
+    }
+  function NotesController($scope){
+      $scope.notes = [];
+      $scope.note = {title:'', body:''};
+      $scope.save = function(){
+        $scope.notes.push($scope.note);
+        $scope.note ={title:'', body:''};
+      }
+    };
+})();
